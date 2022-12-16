93a589434418ef89eac65330e628b351b9079916
rbquick <quickrb@gmail.com>
2022-12-16 18:44:37 -0500

    Initial Commit

---
+++ GameTrackerrbq/GameTrackerrbqApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTrackerrbqApp.swift
+//  GameTrackerrbq
+//
+//  Created by Brian Quick on 2022-12-16.
+//
+
+import SwiftUI
+
+@main
+struct GameTrackerrbqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
